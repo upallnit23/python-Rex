90aedaa487ab0712a69a33e54ee1444139298fa6
upallnit23 <cher_cole@email.com>
2024-05-11 10:34:01 -0500

    adding latest assignment

---
+++ python_Rex.py
@@ -0,0 +1,62 @@
+import re
+
+#Python Regular Expressions Mastery
+#Task 1:Code Correction
+
+text = "Contact emails are: john.doe@example.com and jane.doe@example.com."
+emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}", text)
+print(emails)
+
+text2 = "Contact emails are: linda@example.com and coriander_123@example.com."
+emails2 = re.findall(r"[\w]+@[\w]+\.[A-Z|a-z]{2,}", text2)
+print(emails2)
+
+#Python Regular Expressions Deep Dive
+#Task 1:Email Extraction Enhancement
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+[^exclude]+\.[A-Z|a-z]{2,}\b", text)
+print(emails)
+
+#3. Advanced Text Processing with Python Regex
+#Task 1:Advanced Data Extraction
+
+texture = "Name: John Doe; Age: 30; Occupation: Engineer; Location: New York"
+
+# Extract the Occupation from the text
+
+#found = re.findall("Occupation", texture)
+#for key in texture:
+found = re.findall(r"Occupation:" "[.\w]", texture)
+print(found)
+
+
+    
+
+#4 Python Regex Challenge: Enhancing E-Commerce Operations
+#Task 1:Product Description Keyword Tagging
+
+descriptions = [
+    "Smartphone with 6-inch screen and 128GB memory",
+    "Cotton t-shirt in medium size",
+    "Stainless steel kitchen knife set"]
+
+
+moddescript = re.split(r",", ",".join(descriptions))
+print(moddescript)
+
+key = "Occopation", "t-shirt", "kitchen knife set"
+key1 = "289.99", "10.99", "59.99"
+for i in range(len(moddescript)):
+    moddescript[i] = re.sub(r"[\w.]^[key[i]]*", " ", moddescript[i])
+    print(f"{moddescript[i]} , priced at ${key1[i]}.")
+
+
+
+
+
+
+
+
+
+
